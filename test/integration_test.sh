cb97ad0c89615b5b062e1f8d4780d7e8758d5962
Joe Elliott <jelliott@qsrautomations.com>
2018-12-27 09:54:40 -0600

    Made default a cluster admin to allow reading grafana dashboards.  Super shortcut

--- test/integration_test.sh
+++ test/integration_test.sh
@@ -10,5 +10,8 @@ eval $(minikube docker-env)
 
 docker build .. -t $IMAGE_NAME
 
-# kubectl delete deployment $IMAGE_NAME
+kubectl delete clusterrolebinding --ignore-not-found=true $IMAGE_NAME
+kubectl delete deployment --ignore-not-found=true $IMAGE_NAME
+
+kubectl create clusterrolebinding $IMAGE_NAME --clusterrole=cluster-admin --serviceaccount=default:default
 kubectl run $IMAGE_NAME --image=$IMAGE_NAME --image-pull-policy=Never
