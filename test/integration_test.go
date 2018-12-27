dfcdc1bd015f0d3a6d4a930959de86a71f3f638e
Joe Elliott <jelliott@qsrautomations.com>
2018-12-26 22:01:05 -0600

    First pass integration test

--- test/integration_test.go
+++ test/integration_test.go
@@ -2,11 +2,20 @@ package test
 
 import (
 	"bufio"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"net/url"
 	"os"
 	"os/exec"
+	"reflect"
 	"testing"
+
+	"kubernetes-grafana-controller/pkg/apis/samplecontroller/v1alpha1"
+
+	"github.com/imroc/req"
+	yaml "gopkg.in/yaml.v2"
 )
 
 var (
@@ -23,7 +32,9 @@ func TestMain(m *testing.M) {
 	if !(*nosetup) {
 		setupIntegration()
 	}
+
 	result := m.Run()
+
 	if !(*nosetup) {
 		teardownIntegration()
 	}
@@ -98,6 +109,57 @@ func run(cmdName string, cmdArgs []string) error {
 	return nil
 }
 
+func getGrafanaUrl() (string, error) {
+
+	out, err := exec.Command("minikube", []string{"service", "grafana", "--url", "-p", MINIKUBE_NAME}...).Output()
+	if err != nil {
+		return "", err
+	}
+
+	grafanaUrl := string(out)
+
+	_, err = url.ParseRequestURI(grafanaUrl)
+
+	if err != nil {
+		return "", err
+	}
+
+	return grafanaUrl, nil
+}
+
+func getGrafanaDashboardId(name string) (string, error) {
+
+	out, err := exec.Command("kubectl", []string{"get", "GrafanaDashboard", name, "-o=jsonpath='{.items[0].Status}'"}...).Output()
+	if err != nil {
+		return "", err
+	}
+
+	id := string(out)
+
+	if len(id) == 0 {
+		return "", fmt.Errorf("Grafana Id is empty for %s", name)
+	}
+
+	return id, nil
+}
+
+func areEqualJSON(s1, s2 string) (bool, error) {
+	var o1 interface{}
+	var o2 interface{}
+
+	var err error
+	err = json.Unmarshal([]byte(s1), &o1)
+	if err != nil {
+		return false, fmt.Errorf("Error mashalling string 1 :: %s", err.Error())
+	}
+	err = json.Unmarshal([]byte(s2), &o2)
+	if err != nil {
+		return false, fmt.Errorf("Error mashalling string 2 :: %s", err.Error())
+	}
+
+	return reflect.DeepEqual(o1, o2), nil
+}
+
 //
 // tests
 //
@@ -105,5 +167,63 @@ func run(cmdName string, cmdArgs []string) error {
 func TestDashboardPost(t *testing.T) {
 	fmt.Println("TestDashboardPost")
 
-	t.Error("Fail")
+	var id string
+	var resp *req.Resp
+
+	grafanaUrl, err := getGrafanaUrl()
+	if err != nil {
+		t.Error("Failed to get Grafana URL", err)
+	}
+
+	// create dashboard
+	if err = run("kubectl", []string{"create", "-f", "sample-dashboards.yaml"}); err != nil {
+		t.Error("Failed to create dashboards", err)
+		return
+	}
+
+	// get object status to get grafana id
+	if id, err = getGrafanaDashboardId("test"); err != nil {
+		t.Error("Failed to get id", err)
+		return
+	}
+
+	// GET grafana dashboard with id
+	if resp, err = req.Get(grafanaUrl + "/api/dashboards/uid/" + id); err != nil {
+		t.Error("Failed to get dashboard", err)
+		return
+	}
+
+	status := resp.Response().StatusCode
+	if status >= 300 || status < 200 {
+		t.Error("Get Dashboard status is unsuccessful", resp.Response().Status)
+		return
+	}
+
+	// get file json
+	var fileBytes []byte
+	var grafanaDashboard v1alpha1.GrafanaDashboard
+
+	fileBytes, err = ioutil.ReadFile("sample-dashboards.yaml")
+	if err != nil {
+		t.Error("Error reading sample-dashboards.yaml", err)
+		return
+	}
+
+	err = yaml.Unmarshal(fileBytes, &grafanaDashboard)
+	if err != nil {
+		t.Error("Error unmarshalling grafanaDashboard", err)
+		return
+	}
+
+	var equal bool
+	equal, err = areEqualJSON(grafanaDashboard.Spec.DashboardJSON, resp.String())
+
+	if err != nil {
+		t.Error("Error comparing json", err)
+		return
+	}
+
+	if !equal {
+		t.Error("Dashboard jsons are not equal")
+	}
 }
