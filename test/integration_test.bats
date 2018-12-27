dee931f55defb4aaaf31534afe1e7c9c6f359ad9
Joe Elliott <jelliott@qsrautomations.com>
2018-12-27 16:20:12 -0600

    Built basic bats startup/teardown structure

---
+++ test/integration_test.bats
@@ -0,0 +1,44 @@
+#!/usr/bin/env bats
+
+load setup
+
+setup(){
+    # one time setup
+    if [ "$BATS_TEST_NUMBER" -eq 1 ]; then
+        setupIntegrationTests
+    fi
+
+    kubectl apply -f crd.yaml
+    [ "$?" -eq "0" ]
+
+    kubectl apply -f grafana.yaml
+    [ "$?" -eq "0" ]
+
+    validateGrafanaUrl
+
+    # confirm one time setup worked before every step
+    [ "$SETUP_SUCCEEDED" -eq "1" ]
+}
+
+teardown(){
+    # one time teardown
+    if [ "$BATS_TEST_NUMBER" -eq ${#BATS_TEST_NAMES[@]} ]; then
+        teardownIntegrationTests
+    fi
+
+    kubectl delete -f crd.yaml
+    [ "$?" -eq "0" ]
+
+    kubectl delete -f grafana.yaml
+    [ "$?" -eq "0" ]
+}
+
+@test "addition using bc" {
+  result="$(echo 2+2 | bc)"
+  [ "$result" -eq 4 ]
+}
+
+@test "addition using dc" {
+  result="$(echo 2 2+p | dc)"
+  [ "$result" -eq 4 ]
+}
---
+++ test/setup.bash
@@ -0,0 +1,45 @@
+SETUP_SUCCEEDED=0
+GRAFANA_URL=""
+
+setupIntegrationTests() {
+
+    # ignore failure on these.  they will fail if a minikube cluster does not exist
+	minikube stop
+	minikube delete
+
+    minikube start
+
+	# build dockerfile and deploy to minikube
+	IMAGE_NAME=kubernetes-grafana-test
+
+    eval $(minikube docker-env)
+
+    docker build .. -t $IMAGE_NAME
+
+    kubectl delete clusterrolebinding --ignore-not-found=true $IMAGE_NAME
+    kubectl delete deployment --ignore-not-found=true $IMAGE_NAME
+
+    kubectl create clusterrolebinding $IMAGE_NAME --clusterrole=cluster-admin --serviceaccount=default:default
+    kubectl run $IMAGE_NAME --image=$IMAGE_NAME --image-pull-policy=Never
+
+    SETUP_SUCCEEDED=1
+}
+
+teardownIntegrationTests() {
+    minikube stop
+}
+
+validateGrafanaUrl() {
+    # get grafana url
+    GRAFANA_URL=$(minikube service grafana --url)
+
+    echo "grafana url: " $GRAFANA_URL
+    run curl --silent --output /dev/null --write-out "%{http_code}" $GRAFANA_URL
+    [ "$status" -eq "200"]
+}
+
+getGrafanaDashboardIdByName () {
+    id=$(kubectl get GrafanaDashboard -o=jsonpath='{.items[?(@.metadata.name==\"${$1}\")].status.grafanaUID}')
+
+    return $id
+}
